c69237cdb4342b4811b164095f8f81d1ae1414f5
fuseyjz <35911972+fuseyjz@users.noreply.github.com>
2018-01-29 14:15:56 +0800

    Create Get-CustomSdb.ps1

---
+++ Get-CustomSdb.ps1
@@ -0,0 +1,48 @@
+function Get-CustomSdb {
+<#
+    .SYNOPSIS
+
+        Author: Jayden Zheng (@fuseyjz)
+
+        Checks for custom installed shims database.
+
+    .EXAMPLE
+
+        PS C:\> Get-CustomSdb
+
+        Return those installed sdb registry keys and content.
+#>
+    Write-Host "Query: HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Custom `n"
+
+    Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Custom" -Recurse | ForEach-Object {
+
+
+        $GetName = $_.Name
+        $SplitName = $GetName -split 'Custom\\'
+        $KeyName = $SplitName[1]
+        $GetProperty = $_.Property
+
+        if ($GetProperty) {
+            Write-Host "Key Name: $KeyName"
+            Write-Host "Sdb Name: $GetProperty `n"
+        }
+
+    }
+
+    Write-Host "============================================== `n"
+    Write-Host "Query: HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\InstalledSDB `n"
+
+    Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\InstalledSDB" -Recurse | Get-ItemProperty |  ForEach-Object {
+
+        $GetDesc = $_.DatabaseDescription
+        $GetPath = $_.DatabasePath
+        $GetCont = Get-Content -Path $GetPath | Select-Object -Last 1
+
+        if ($GetDesc) {
+            Write-Host "Description: $GetDesc"
+            Write-Host "Path: $GetPath"
+            Write-Host "Content of sdb: `n"
+            Write-Host "$GetCont `n"
+        }
+    }
+}
