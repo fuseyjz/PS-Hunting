2a1c5ad38dabffca6df0431252e604d9d23a8f8a
fuseyjz <35911972+fuseyjz@users.noreply.github.com>
2018-02-24 15:32:15 +0800

    Update Get-CustomSdb.ps1

--- Get-CustomSdb.ps1
+++ Get-CustomSdb.ps1
@@ -4,43 +4,76 @@ function Get-CustomSdb {
 
         Author: Jayden Zheng (@fuseyjz)
         
-        Company: Countercept (@countercept)
-
         Checks for custom installed shims database.
 
     .EXAMPLE
 
         PS C:\> Get-CustomSdb
 
-        Return those installed sdb registry keys.
+        Return those installed sdb registry keys and content of the sdb.
 #>
-    Write-Host "Checking: HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Custom"
 
-    Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Custom" -Recurse | ForEach-Object {
+    #Function from https://cyber-defense.sans.org/blog/2010/02/11/powershell-byte-array-hex-convert
+    function Convert-HexStringToByteArray
+    {
+        [CmdletBinding()]
+        Param ( [Parameter(Mandatory = $True, ValueFromPipeline = $True)] [String] $String )
 
+        #Clean out whitespaces and any other non-hex crud.
+        $String = $String.ToLower() -replace '[^a-f0-9\\,x\-\:]',"
 
-        $GetName = $_.Name
-        $SplitName = $GetName -split 'Custom\\'
-        $KeyName = $SplitName[1]
-        $GetProperty = $_.Property
+        #Try to put into canonical colon-delimited format.
+        $String = $String -replace '0x|\x|\-|,',':'
 
-        if ($GetProperty) {
-            Write-Host "Key Name: $KeyName"
-            Write-Host "Sdb Name: $GetProperty `n"
-        }
+        #Remove beginning and ending colons, and other detritus.
+        $String = $String -replace '^:+|:+$|x|\',"
 
+        #Maybe there's nothing left over to convert...
+        if ($String.Length -eq 0) { ,@() ; return }
+
+        #Split string with or without colon delimiters.
+        if ($String.Length -eq 1)
+            { ,@([System.Convert]::ToByte($String,16)) }
+        elseif (($String.Length % 2 -eq 0) -and ($String.IndexOf(":") -eq -1))
+            { ,@($String -split '([a-f0-9]{2})' | foreach-object { if ($_) {[System.Convert]::ToByte($_,16)}}) }
+        elseif ($String.IndexOf(":") -ne -1)
+            { ,@($String -split ':+' | foreach-object {[System.Convert]::ToByte($_,16)}) }
+        else
+            { ,@() }
     }
-
-    Write-Host "Checking: HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\InstalledSDB"
-
+   
     Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\InstalledSDB" -Recurse | Get-ItemProperty |  ForEach-Object {
 
         $GetDesc = $_.DatabaseDescription
         $GetPath = $_.DatabasePath
+        $GetChildName = $_.PSChildName
+        
+        Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Custom" -Recurse | Where-Object {$_.Property -match $GetChildName} | ForEach-Object {
+            $GetName = $_.Name
+            $SplitName = $GetName -split 'Custom\\'
+            $KeyName = $SplitName[1]
+            $GetProperty = $_.Property
 
-        if ($GetDesc) {
+            if ($GetProperty) {
+                Write-Host "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Custom"
+                Write-Host "Key Name: $KeyName"
+                Write-Host "Sdb Name: $GetProperty `n"
+            }
+        }
+
+        $bytes = [System.IO.File]::ReadAllBytes($GetPath)
+        $hex = ($bytes | Format-Hex | Select-Object -Expand Bytes | ForEach-Object { '{0:x2}' -f $_ }) -join ''
+        $index = $hex.IndexOf("0970")
+        $chop = $hex.Substring($index)
+        $out = Convert-HexStringToByteArray $chop
+        $output = [System.Text.Encoding]::ASCII.GetString($out)
+
+        if ($GetPath) {
+            Write-Host "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\InstalledSDB"
             Write-Host "Description: $GetDesc"
-            Write-Host "Path: $GetPath `n"
+            Write-Host "Path: $GetPath"
+            Write-Host "Content of sdb:"
+            Write-Host "$output `n"
         }
     }
 }
