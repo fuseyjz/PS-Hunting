154c67770690acc8682895a82123fa302f4bcff0
fuseyjz <35911972+fuseyjz@users.noreply.github.com>
2018-05-22 15:40:09 +0800

    Update Get-CustomSdb.ps1

--- Get-CustomSdb.ps1
+++ Get-CustomSdb.ps1
@@ -10,38 +10,9 @@ function Get-CustomSdb {
 
         PS C:\> Get-CustomSdb
 
-        Return those installed sdb registry keys and content of the sdb.
+        Return those installed sdb registry keys.
 #>
-
-    # from https://cyber-defense.sans.org/blog/2010/02/11/powershell-byte-array-hex-convert
-    function Convert-HexStringToByteArray
-    {
-        [CmdletBinding()]
-        Param ( [Parameter(Mandatory = $True, ValueFromPipeline = $True)] [String] $String )
-
-        #Clean out whitespaces and any other non-hex crud.
-        $String = $String.ToLower() -replace '[^a-f0-9\\,x\-\:]',"
-
-        #Try to put into canonical colon-delimited format.
-        $String = $String -replace '0x|\x|\-|,',':'
-
-        #Remove beginning and ending colons, and other detritus.
-        $String = $String -replace '^:+|:+$|x|\',"
-
-        #Maybe there's nothing left over to convert...
-        if ($String.Length -eq 0) { ,@() ; return }
-
-        #Split string with or without colon delimiters.
-        if ($String.Length -eq 1)
-            { ,@([System.Convert]::ToByte($String,16)) }
-        elseif (($String.Length % 2 -eq 0) -and ($String.IndexOf(":") -eq -1))
-            { ,@($String -split '([a-f0-9]{2})' | foreach-object { if ($_) {[System.Convert]::ToByte($_,16)}}) }
-        elseif ($String.IndexOf(":") -ne -1)
-            { ,@($String -split ':+' | foreach-object {[System.Convert]::ToByte($_,16)}) }
-        else
-            { ,@() }
-    }
-   
+  
     Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\InstalledSDB" -Recurse | Get-ItemProperty |  ForEach-Object {
 
         $GetDesc = $_.DatabaseDescription
@@ -61,19 +32,10 @@ function Get-CustomSdb {
             }
         }
 
-        $bytes = [System.IO.File]::ReadAllBytes($GetPath)
-        $hex = ($bytes | Format-Hex | Select-Object -Expand Bytes | ForEach-Object { '{0:x2}' -f $_ }) -join ''
-        $index = $hex.IndexOf("0970")
-        $chop = $hex.Substring($index)
-        $out = Convert-HexStringToByteArray $chop
-        $output = [System.Text.Encoding]::Unicode.GetString($out)
-
         if ($GetPath) {
             Write-Host "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\InstalledSDB"
             Write-Host "Description: $GetDesc"
             Write-Host "Path: $GetPath"
-            Write-Host "Content of sdb:"
-            Write-Host "$output `n"
         }
     }
 }
