41a4ecc816322256d4a76b5679b53d7d98ec9ede
fuseyjz <35911972+fuseyjz@users.noreply.github.com>
2018-05-28 16:04:43 +0800

    Update Get-MeterpreterC2.ps1

--- Get-MeterpreterC2.ps1
+++ Get-MeterpreterC2.ps1
@@ -1,1 +1,3 @@
-https://github.com/countercept/memory-carving-scripts/blob/master/Get-MeterpreterC2.ps1
+Blog: https://www.countercept.com/our-thinking/retrieving-meterpreter-c2-from-memory/
+
+Script link: https://github.com/countercept/memory-carving-scripts/blob/master/Get-MeterpreterC2.ps1
