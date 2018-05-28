f5f467726e95b8627a91f90ee4bc04ff21a8fce5
fuseyjz <35911972+fuseyjz@users.noreply.github.com>
2018-05-28 16:02:06 +0800

    Update meterc2.py

--- meterc2.py
+++ meterc2.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
+'''
+Blog: https://www.countercept.com/our-thinking/retrieving-meterpreter-c2-from-memory/
+
+Python version of Get-MeterpreterC2.ps1
+Link: https://github.com/countercept/memory-carving-scripts/blob/master/Get-MeterpreterC2.ps1
+
+'''
+
 import binascii
 import sys
 
