a1bf338da8947700d6b3489062474eaf95b16f77
fuseyjz <35911972+fuseyjz@users.noreply.github.com>
2018-06-28 07:22:57 +0800

    Delete get_meterpreter_c2.py

--- get_meterpreter_c2.py
+++
@@ -1,4 +0,0 @@
-Blog: https://www.countercept.com/our-thinking/retrieving-meterpreter-c2-from-memory/
-
-Python version of Get-MeterpreterC2.ps1
-Link: https://github.com/countercept/memory-carving-scripts/blob/master/get_meterpreter_c2.py
