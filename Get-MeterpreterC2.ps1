b929fe879981e57168d599ad2f6359d4cc028736
fuseyjz <35911972+fuseyjz@users.noreply.github.com>
2018-06-28 07:22:44 +0800

    Delete Get-MeterpreterC2.ps1

--- Get-MeterpreterC2.ps1
+++
@@ -1,3 +0,0 @@
-Blog: https://www.countercept.com/our-thinking/retrieving-meterpreter-c2-from-memory/
-
-Script link: https://github.com/countercept/memory-carving-scripts/blob/master/Get-MeterpreterC2.ps1
